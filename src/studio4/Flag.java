b7092b495d8c333292077d0ccc0913440f33858e
tavialiu <tavialiu@localhost>
2024-02-27 21:48:07 -0600

    Studio 4

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,30 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		Color faceColor = new Color(232, 220, 202);
+		StdDraw.setPenColor(faceColor);
+		StdDraw.filledEllipse(.5, .7, .2, .15);
+		StdDraw.filledEllipse(.5, .3, .1, .075);//bottom circle
+		double[]x={0.3,.4,.6,0.7,};
+		double[]y={0.7,.3,.3,0.7};
+		StdDraw.filledPolygon(x, y);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.ellipse(.4, .7, .06, .09);
+		StdDraw.ellipse(.6, .7, .06, .09);
+		Color eyeColor = new Color(88,57,39);
+		StdDraw.setPenColor(eyeColor);
+		StdDraw.filledEllipse(.4, .7, .06, .08);
+		StdDraw.filledEllipse(.6, .7, .06, .08);
+		StdDraw.setPenColor(Color.WHITE);
+		StdDraw.filledEllipse(.42, .7, .02, .022);
+		StdDraw.filledEllipse(.58, .7, .02, .022);
+		StdDraw.setPenColor(Color.BLACK);
+		StdDraw.filledEllipse(.45, .275, .05, .03);
+		StdDraw.filledEllipse(.55, .275, .05, .03);
+		StdDraw.filledEllipse(.5, .250, .06, .03);
+		StdDraw.setPenColor(faceColor);
+		StdDraw.filledEllipse(.275, .6, .065, .15);
+		StdDraw.filledEllipse(.725, .6, .065, .15);
 		
 	}
 }
